a6fc42628a382d31e352b682ba6aa7f6430db89f
olegmrnv <olegmrnv@gmail.com>
2019-06-11 14:41:49 -0500

    started on main tip calc function, added comments

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,14 @@
+
+
+
+
+// Getting value of total bill 
 var bill = d3.select("");
+// checking if bill needs to be split between several guests
+var guestNumber = d3.select("");
+
+// main function that will calculate tip amount
+
+function to_tip(totalCost, splitBetween){
+
+}
