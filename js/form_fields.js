63813f4f49404a56151d77b5a588ae3986f668e1
olegmrnv <olegmrnv@gmail.com>
2019-06-16 17:37:53 -0500

    added javascript file to enable and disable some fields in the form

---
+++ js/form_fields.js
@@ -0,0 +1,23 @@
+document.getElementById('split').onchange = function () {
+    document.getElementById('numberOFpayes').disabled = !this.checked;
+};
+
+document.getElementById('other').onchange = function () {
+    document.getElementById('other_persent').disabled = !this.checked;
+};
+
+document.getElementById('twenty').onchange = function () {
+    document.getElementById('other_persent').disabled = true;
+};
+
+document.getElementById('eighteen').onchange = function () {
+    document.getElementById('other_persent').disabled = true;
+};
+
+document.getElementById('fifteen').onchange = function () {
+    document.getElementById('other_persent').disabled = true;
+};
+
+document.getElementById('ten').onchange = function () {
+    document.getElementById('other_persent').disabled = true;
+};
